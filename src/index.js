a6983b2be7c4bdb97af788004e3385cc4109d63e
Angelo Teixeira <imnotteixeira@gmail.com>
2020-04-25 00:21:22 +0100

    Added metrics logging on simulation end

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const config = require("./config");
 const { displayMatrix } = require("./utils");
-const { init, simulate } = require("@imnotteixeira/covid-19-simulator");
+const { init, simulate, MetricsService } = require("@imnotteixeira/covid-19-simulator");
 
 const simulationData = init({
     populationSize: config.POPULATION_SIZE,
@@ -10,6 +10,15 @@ const simulationData = init({
     hospitalCapacity: config.HOSPITAL_CAPACITY,
 });
 
+if (simulationData.population.length > 100) {
+    console.info("Population too big. Will not render population on each step.");
+}
+
+MetricsService.subscribe("carrier-count");
+MetricsService.subscribe("dead-count");
+MetricsService.subscribe("cured-count");
+MetricsService.subscribe("hospitalized-count");
+
 simulate(simulationData, config.MAX_STEPS, {
     stepEnd: (simulationData) => {
         if (simulationData.population.length <= 100) {
@@ -17,3 +26,8 @@ simulate(simulationData, config.MAX_STEPS, {
         }
     },
 });
+
+const metricValues = MetricsService.export();
+for (const metric of metricValues) {
+    console.info(`[${metric.id}] ${metric.data}`);
+}
